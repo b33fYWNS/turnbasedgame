ae35de1c36a451c73ee461ef0951e9a8fda0c23d
b33f90 <168454641+b33f90@users.noreply.github.com>
2024-05-02 14:02:35 -0400

    Update Damage_Number_Display.gd
    Proper display and hide for WEAK label

--- UI/Damage_Number_Display.gd
+++ UI/Damage_Number_Display.gd
@@ -3,18 +3,24 @@ class_name DamageNumber
 
 @onready var label = $Container/Label
 @onready var animation_player = $AnimationPlayer
+@onready var weak_label = $Container/WeakLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	var battle_unit = get_parent()
 
-func set_damage_number(damage_amount : int) -> void:
+func set_damage_number(damage_amount : int, is_weak: bool = false) -> void:
 	label.text = str(damage_amount)
+	if is_weak:
+		set_weak_text()
 	animation_player.play("Number_Fade")
 	await animation_player.animation_finished
 	queue_free()
-func set_critical_damage_number(damage_amount : int) -> void:
+	
+func set_critical_damage_number(damage_amount : int, is_weak: bool = false) -> void:
 	label.text = str(damage_amount) + "!!"
+	if is_weak:
+		set_weak_text()
 	animation_player.play("Number_Fade")
 	await animation_player.animation_finished
 	queue_free()

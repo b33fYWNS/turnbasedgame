db8789dd85d51b3ea1684afb7949b91eb89198b2
b33f90 <168454641+b33f90@users.noreply.github.com>
2024-05-03 11:14:24 -0400

    Update battle_unit.gd
    Updated for new turn-based speed system

--- Battle/battle_unit.gd
+++ Battle/battle_unit.gd
@@ -4,7 +4,6 @@ class_name BattleUnit
 const ATTACK_OFFSET = 48
 const KNOCKBACK_AMOUNT = 24
 
-var asyncTurnPool : AsyncTurnPool = ReferenceStash.asyncTurnPool
 var defend : bool = false : set = set_defend
 var damage_number_scene = preload("res://UI/damage_number_display.tscn")
 
@@ -20,6 +19,9 @@ var damage_number_scene = preload("res://UI/damage_number_display.tscn")
 
 signal critical_hit
 signal normal_hit
+signal weakness_hit()
+signal turn_finished
+signal enemy_defeated
 
 func set_stats(value : ClassStats) -> void:
 	stats = value
@@ -33,49 +35,53 @@ func set_defend(value : bool) -> void:
 	battle_shield.visible = defend
 	if defend == true:
 		defend_sfx.play()
-
-func melee_attack(target : BattleUnit, battle_action: MeleeBattleAction) -> void:
-	asyncTurnPool.add(self)
-	z_index = 10
-	battle_animations.play("Approach")
-	var target_position := target.global_position.move_toward(global_position, ATTACK_OFFSET)
-	var animation_duration := battle_animations.get_current_animation_length()
-	interpolate_position(global_position, target_position, animation_duration)
-	await battle_animations.animation_finished
-	deal_damage(target, battle_action)
-	target.take_hit(self)
-	battle_animations.play("Melee")
-	await battle_animations.animation_finished
-	battle_animations.play("Return")
-	animation_duration = battle_animations.get_current_animation_length()
-	interpolate_position(global_position,root_position,animation_duration)
-	await battle_animations.animation_finished
-	battle_animations.play("Idle")
-	z_index = 0
-	asyncTurnPool.remove(self)
+		
+func take_turn() -> void:
+	#print("BattleUnit: take_turn() called for", self)
+	if is_player_controlled():
+		var selected_action = await ReferenceStash.battle_menu_manager.show_battle_menu()
+		perform_action(selected_action)
+	else:
+		var battle_action = ReferenceStash.ai._select_battle_action(stats)
+		perform_action(battle_action)
+	emit_signal("turn_finished")
+	#print("BattleUnit: turn_finished signal emitted for", self)
+	
+func is_player_controlled() -> bool:
+	return true
+	
+func perform_action(action) -> void:
+	var target = BattleUnit
+	if action is RangedBattleAction:
+		ranged_attack(target, action)
+	elif action is EnemyBattleAction:
+		enemy_attack(target, action)
+	elif action is BasicBattleAction:
+		basic_attack(target, action)
+	elif action is Item:
+		use_item(target, action)
+	await get_tree().create_timer(3.0).timeout
 	
 func ranged_attack(target : BattleUnit, battle_action : RangedBattleAction) -> void:
-	asyncTurnPool.add(self)
+	
 	battle_animations.play("RangedAnticipation")
 	await battle_animations.animation_finished
 	
-	
+	battle_animations.play("RangedRelease")
+	await battle_animations.animation_finished
 	var projectile = battle_action.projectile.instantiate()
 	get_tree().current_scene.add_child(projectile)
 	projectile.global_position = battle_animations.get_emission_position()
 	projectile.move_to(target)
+	
 	await projectile.contact
 	
 	deal_damage(target, battle_action)
 	target.take_hit(self)
-	
-	battle_animations.play("RangedRelease")
-	await battle_animations.animation_finished
 	battle_animations.play("Idle")
-	asyncTurnPool.remove(self)
+	
 	
 func enemy_attack(target : BattleUnit, battle_action : EnemyBattleAction) -> void:
-	asyncTurnPool.add(self)
 	battle_animations.play("Attack_Anticipation")
 	await battle_animations.animation_finished
 	
@@ -91,10 +97,10 @@ func enemy_attack(target : BattleUnit, battle_action : EnemyBattleAction) -> voi
 	
 	await battle_animations.animation_finished
 	battle_animations.play("Idle")
-	asyncTurnPool.remove(self)
+	
 	
 func basic_attack(target : BattleUnit, battle_action : BasicBattleAction) -> void:
-	asyncTurnPool.add(self)
+	
 	battle_animations.play("Attack_Anticipation")
 	await battle_animations.animation_finished
 	
@@ -109,10 +115,10 @@ func basic_attack(target : BattleUnit, battle_action : BasicBattleAction) -> voi
 	
 	await battle_animations.animation_finished
 	battle_animations.play("Idle")
-	asyncTurnPool.remove(self)
+	
 	
 func use_item(target: BattleUnit, item : Item) -> void:
-	asyncTurnPool.add(self)
+	
 	battle_animations.play("Attack")
 	await battle_animations.animation_finished
 	stats.health += item.heal_amount
@@ -120,31 +126,32 @@ func use_item(target: BattleUnit, item : Item) -> void:
 	#battle_animations.play("Idle")
 	#await battle_animations.animation_finished
 	battle_animations.play("Idle")
-	asyncTurnPool.remove(self)
+	emit_signal("turn_finished")
+	
 	
 func deal_damage(target: BattleUnit, battle_action : DamageBattleAction) -> void:
-	print("Attacker's Stats:")
-	print("Attack: ", stats.attack)
-	print("Crit Chance: ", stats.crit_chance)
+	#print("Attacker's Stats:")
+	#print("Attack: ", stats.attack)
+	#print("Crit Chance: ", stats.crit_chance)
 	
-	print("Defender's Stats:")
-	print("Defense: ", target.stats.defense)
-	print("Weaknesses: ", target.stats.weaknesses)
+	#print("Defender's Stats:")
+	#print("Defense: ", target.stats.defense)
+	#print("Weaknesses: ", target.stats.weaknesses)
 	
 	var base_damage = (((stats.attack*1.1 - (target.stats.defense * 1.1))*battle_action.damage/2))
-	print("Base Damage: ", base_damage)
+	#print("Base Damage: ", base_damage)
 	var is_weak = target.stats.weaknesses.has(battle_action.element)
-	print("Is Weak: ", is_weak)
+	#print("Is Weak: ", is_weak)
 	var damage = base_damage
 	if is_weak:
 		damage *= 2
-		print("Weakness Multiplier Applied")
+		#print("Weakness Multiplier Applied")
 		weakness_hit.emit()
 	var is_critical = randf() < stats.crit_chance
 	if is_critical:
 		damage *= 2
-		print("Critical Hit Multiplier Applied")
-	print("Final Damage: ", damage)
+		#print("Critical Hit Multiplier Applied")
+	#print("Final Damage: ", damage)
 
 	var damage_number = damage_number_scene.instantiate()
 	add_child(damage_number)
@@ -154,19 +161,22 @@ func deal_damage(target: BattleUnit, battle_action : DamageBattleAction) -> void
 	else:
 		damage_number.set_damage_number(damage, is_weak)
 	target.stats.health -= damage
-	print("Defender's Health: ", target.stats.health)
-	
+	#print("Defender's Health: ", target.stats.health)
+	#emit_signal("turn_finished")
 func take_hit(attacker: BattleUnit) -> void:
 	hit_sfx.play()
-	asyncTurnPool.add(self)
+	
 	var knockback_position := global_position.move_toward(attacker.global_position, -KNOCKBACK_AMOUNT)
 	interpolate_position(global_position, knockback_position, .2, Tween.TRANS_CIRC, Tween.EASE_OUT)
 	
 	if stats.health == 0:
+		
 		battle_animations.play("Death")
 		await battle_animations.animation_finished
-		asyncTurnPool.remove(self)
+		ReferenceStash.turnManager.remove_battle_unit(self)
+		emit_signal("enemy_defeated")
 		queue_free()
+		emit_signal("turn_finished")
 		return
 	else:
 		battle_animations.play("Hit")
@@ -174,7 +184,8 @@ func take_hit(attacker: BattleUnit) -> void:
 		battle_animations.play("Idle")
 		
 	interpolate_position(global_position, root_position, .2, Tween.TRANS_CIRC)
-	asyncTurnPool.remove(self)
+	print("Emitting turn_finished signal.")
+	emit_signal("turn_finished")
 	
 func interpolate_position(start: Vector2, end : Vector2, duration : float, trans : int = Tween.TRANS_LINEAR, easing : int = Tween.EASE_IN_OUT) -> void:
 	var tween = create_tween().set_trans(trans).set_ease(easing)
